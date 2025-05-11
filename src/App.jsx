bb20cbb2096870b4d8599ea1a59dd1cc3ddfcf0a
Divyanshu Pandey <divyanshupandey9329@gmail.com>
2025-05-11 09:08:29 +0530

    commit to add Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 // import ChangeColor from './components/ChangeColor'
 import PasswordGenerator from './components/PasswordGenerator'
+import NewCard from './components/NewCard'
 
 function App() {
 
   return (
       <>
       {/* <ChangeColor/> */}
-      <PasswordGenerator/>
+      {/* <PasswordGenerator/> */}
+      <NewCard/>
       </>
   )
 }
